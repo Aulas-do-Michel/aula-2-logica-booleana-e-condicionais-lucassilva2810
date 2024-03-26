ccfb608ce12ac13ba17cf907a0331d512c344bac
Lucas Rafael da Silva <163673380+lucassilva2810@users.noreply.github.com>
2024-03-25 21:07:59 -0300

    Subindo exercicio 1

--- exercicio_1.py
+++ exercicio_1.py
@@ -17,3 +17,9 @@ Digite um número:
 
 Dica: Lembre do comando de resto da divisão inteira!
 """
+numero_digitado = int (input ("Digite um número:"))
+
+if numero_digitado % 2 == 0:
+    print ("Par")
+else:
+    print ("Ímpar")
